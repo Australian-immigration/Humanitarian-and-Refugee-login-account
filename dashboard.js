33f6f5af4287b919fa740b653cd8fdcac0d1dec0
Australian-immigration <ahmadjan.file@gmail.com>
2025-07-20 05:45:31 -0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -3,5 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!user) {
     window.location.href = 'login.html';
   }
-  document.querySelector('.content h1').innerText = `خوش آمدید ${user.firstName} ${user.lastName}`;
+
+  document.querySelector('.content h1').innerText = `Welcome ${user.firstName} ${user.lastName}`;
+
+  const logoutBtn = document.getElementById('logoutBtn');
+  if (logoutBtn) {
+    logoutBtn.addEventListener('click', () => {
+      localStorage.removeItem('user');
+      window.location.href = 'login.html';
+    });
+  }
+
+  const btn1 = document.getElementById('btn1');
+  if (btn1 && user.dashboardLinks && user.dashboardLinks.finalResult) {
+    btn1.href = user.dashboardLinks.finalResult;
+  }
 });
